34fb010541fd2d1208c501f569618331cee1322f
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-04-15 17:11:01 +0200

    Updated

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
-
-
 const displayProducts = async () => {
-  const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
-headers: {
-"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmZhNWE5MDIzOTAwMTVkOTY1ZGIiLCJpYXQiOjE2NDk4NTIzMjUsImV4cCI6MTY1MTA2MTkyNX0.byIP9sGJBEu1KNeVEtoF2tsWgEOtohWDDZd-GgY2rUQ"
-}
-})
-  const products = await response.json();
-  console.log(products);
+  const response = await fetch(
+    'https://striveschool-api.herokuapp.com/api/product/',
+    {
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmZhNWE5MDIzOTAwMTVkOTY1ZGIiLCJpYXQiOjE2NDk4NTIzMjUsImV4cCI6MTY1MTA2MTkyNX0.byIP9sGJBEu1KNeVEtoF2tsWgEOtohWDDZd-GgY2rUQ',
+      },
+    },
+  )
+  const products = await response.json()
+  console.log(products)
 
-  let row = document.querySelector(".row")
-  
+  let row = document.querySelector('.row')
 
-
-  
-
-  products.forEach(product => {
-   let col = document.createElement("div")
-   col.className = "col-3 mb-5 image-fluid" 
+  products.forEach((product) => {
+    let col = document.createElement('div')
+    col.className = 'col-3 mb-5 image-fluid'
     col.innerHTML = `<div class="card">
           
           <div class="card-body">
-            <h5 class="card-title">${product.brand}</h5>
+          <div class="d-flex justify-content-center">
+          <img src="${product.imageUrl}" alt="" height="100px" weight="100px" />
+          </div>
+          
+            <h5 class="card-title">${product.name}</h5>
             
-            <a href="#" class="btn btn-primary">${product.price} €</a>
-            <a href="./details.html?productID=${product._id}" class="btn btn-primary float-right">VIEW</a>
+            <div class="d-flex justify-content-between align-items-center">
+            <p class="mb-0 text-danger font-weight-bold lead ">${product.price} €</p>
+            <a href="./details.html?productID=${product._id}" class="btn btn-info">VIEW</a>
+            </div>
 
           </div>
         </div>`
 
-        row.appendChild(col)
-  });
-
-};
-
+    row.appendChild(col)
+  })
+}
 
 // <<<<<<<<<<<< back offine >>>>>>>>>>
 
-
-
-
-
-
-
-
-
-
 window.onload = () => {
-  displayProducts();
-};
-
+  displayProducts()
+}
